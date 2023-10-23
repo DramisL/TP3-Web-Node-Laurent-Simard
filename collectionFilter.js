8b5b8e32f285b2fb333df70dd142938afcf788a8
DramisL <laurentsimard45@hotmail.com>
2023-10-23 17:45:26 -0400

    Finished TP

--- collectionFilter.js
+++ collectionFilter.js
@@ -9,64 +9,102 @@ export default class CollectionFilter {
 
 
 
-    get(){
+    get() {
         //Parametre a ajouter qui ne sont pas inclus dans les parametre du modele
         //sort, field, limit et offset
         let resultList = this.objectList;
-        if (this.params != null){
+        if (this.params != null) {
             for (const [key, value] of Object.entries(this.params)) {
-                let updatedResultList;
-                if (this.containExistingField(key)){
-                    updatedResultList = this.getSpecificObjParamQuery(key, value);
-            }else if (key.toLocaleLowerCase() == "sort"){
-                let splitActionValue = this.defineSortOrder(value);
-                if (splitActionValue != -1){
-                    updatedResultList = this.applySort(value.split(',')[0], splitActionValue)
-                }else{
+                let updatedResultList = [];
+                if (this.containExistingField(key)) {
+                    updatedResultList = this.getSpecificObjParamQuery(key, value, resultList);
+                } else if (key.toLocaleLowerCase() == "sort") {
+                    let splitActionValue = this.defineSortOrder(value);
+                    if (splitActionValue != -1) {
+                        updatedResultList = this.applySort(value.split(',')[0], splitActionValue, resultList)
+                    } else {
+                        updatedResultList = resultList;
+                    }
+
+                } else if (key.toLocaleLowerCase() == "field") {
+                    let fieldToInclude = this.params[key.toLocaleLowerCase()].split(",")
+                    resultList.forEach(element => {
+                        let newEntry = {};
+                        for (let i = 0; i < fieldToInclude.length; i++) {
+                            newEntry[fieldToInclude[i]] = element[fieldToInclude[i]];
+                        }
+
+                        let containsDuplicate = false;
+                        updatedResultList.forEach(entry => {
+                            if (this.equals(entry, newEntry)) {
+                                containsDuplicate = true
+                            }
+                        });
+
+                        if (!containsDuplicate) {
+                            updatedResultList.push(newEntry)
+                        }
+                    });
+                } else if (key.toLocaleLowerCase() == "limit") {
+                    if ("offset" in this.params) {
+                        for (let n = this.params[key] * this.params["offset"]; n < (parseInt(this.params[key]) * parseInt(this.params["offset"])) + parseInt(this.params[key]); n++) {
+                            updatedResultList.push(resultList[n]);
+                        }
+                    } else {
+                        for (let n = 0; n < parseInt(this.params[key]); n++) {
+                            updatedResultList.push(resultList[n]);
+                        }
+                    }
+                } else {
                     updatedResultList = resultList;
                 }
-                
-            } else if (key.toLocaleLowerCase() == "limit"){
-
-            if ("offset" in this.params){
-
-                
+                resultList = updatedResultList;
             }
-            }else if (key.toLocaleLowerCase() == "field"){
-
-            }else {
-                updatedResultList = resultList;
-            }
-
-            resultList = updatedResultList;
-            
-        }   
-        return resultList;
-    } else {
-        return this.objectList;
-    }
+            return resultList;
+        } else {
+            return this.objectList;
+        }
 
     }
 
-
-    containExistingField(targetField){
+    //Code tirer de : https://medium.com/geekculture/object-equality-in-javascript-2571f609386e
+    equals(ox, oy) {
+        let propsX = Object.getOwnPropertyNames(ox);
+        let propsY = Object.getOwnPropertyNames(oy);
+        if (propsX.length != propsY.length) {
+            return false;
+        }
+        for (var i = 0; i < propsX.length; i++) {
+            let valX = ox[propsX[i]];
+            let valY = oy[propsY[i]];
+            let isObjects = this.isObject(valX) && this.isObject(valY);
+            if (isObjects && !this.equal(valX, valY) || !isObjects && valX !== valY) {
+                return false;
+            }
+        }
+        return true;
+    }
+    isObject(object) {
+        return object != null && typeof object === 'object';
+    }
+    containExistingField(targetField) {
         let doesContainField = false;
-        for (let i = 0; i < this.model.fields.length && !doesContainField; i++){
-            if (targetField == this.model.fields[i]["name"]){
+        for (let i = 0; i < this.model.fields.length && !doesContainField; i++) {
+            if (targetField == this.model.fields[i]["name"]) {
                 doesContainField = true;
             }
         }
         return doesContainField;
     }
 
-    defineSortOrder(value){
+    defineSortOrder(value) {
         let splitSortValue = value.split(",");
         if (this.containExistingField(splitSortValue[0])) {
-            if (splitSortValue.length == 1){
+            if (splitSortValue.length == 1) {
                 return 0
-            } else if (splitSortValue[1] == "desc"){
+            } else if (splitSortValue[1] == "desc") {
                 return 1
-            }else {
+            } else {
                 return -1
             }
         }
@@ -75,23 +113,49 @@ export default class CollectionFilter {
         }
     }
 
-    getSpecificObjParamQuery(key, value){
+    getSpecificObjParamQuery(key, value, resultList) {
+
+        let newList = [];
+        resultList.forEach(element => {
+            if (element[key] !== undefined) {
+                let splitString = value.split("*");
+                if (splitString.length == 1 && splitString[0] === value) {
+                    newList.push(element);
+                } else if (splitString.length == 3 && element[key].includes(splitString[1])) {
+                    newList.push(element)
+                } else if (splitString.length == 2) {
+                    let isRespectingSearchConditions = true;
+                    if (splitString[0] != "" && !(element[key].startsWith(splitString[0]))) {
+                        isRespectingSearchConditions = false;
+                    }
+                    if (splitString[1] != "" && !(element[key].endsWith(splitString[1]))) {
+                        isRespectingSearchConditions = false;
+                    }
+                    if (isRespectingSearchConditions) {
+                        newList.push(element);
+                    }
+                }
+            } else {
+                return resultList;
+            }
+        })
+        return newList;
     }
 
-    applySort(field, isDesc){
-        let sortedList = this.objectList;
-        for (let i = 0; i < this.objectList.length; i++){
-            for (let j = i; j < this.objectList.length; j++){
-                if (this.innerCompare(sortedList[i][field],sortedList[j][field]) > 0){
+    applySort(field, isDesc, resultList) {
+        let sortedList = resultList;
+        for (let i = 0; i < resultList.length; i++) {
+            for (let j = i; j < resultList.length; j++) {
+                if (this.innerCompare(sortedList[i][field], sortedList[j][field]) > 0) {
                     let temp = sortedList[i];
-                    sortedList[i]= sortedList[j];
+                    sortedList[i] = sortedList[j];
                     sortedList[j] = temp;
                 }
             }
         }
-        if (isDesc){
+        if (isDesc) {
             return sortedList.reverse()
-        }else{
+        } else {
             return sortedList;
         }
     }
